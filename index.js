7424d770e729335c34bafda5aa8eeb4a960bf063
DESKTOP-47BPED7\AndyWu <andywuchin@gmail.com>
2023-10-10 13:12:20 +0800

    test port

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ wss.on('connection', function connection(ws) {
   connections.push(ws)
 });
 
-server.listen(4000, () => {
+server.listen(3000, () => {
   console.log(`websocket listening on ${port}`);
 });
 
