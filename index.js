2a9125df625e9465f0f725c80e732fa5eba49bbc
Andy CW Wu - LLX <andy_cw_wu@wistron.com>
2023-10-12 10:20:40 +0800

    add deal answer

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const http = require('http');
 const path = require('path');
 const WebSocket = require('ws');
 
-var index = 0;
-var BULLETS = '';
-var USER_AVATAR = '';
+let questionIdx = 0;
+let userPool = [];
+let answerPool = [];
 
 //---------------
 // line sdk
@@ -27,7 +27,7 @@ const client = new line.Client(config);
 //---------------
 // create Express app
 const app = express();
-//app.use(express.json());
+app.use(express.json());
 
 // register a webhook handler with middleware
 app.post('/webhook', line.middleware(config), (req, res) => {
@@ -47,6 +47,26 @@ app.listen(port, () => {
 });
 
 //---------------
+// api
+//---------------
+
+app.get('/status', (req, res) => {
+  //const id = req.query.id;//target question id
+  res.send(questionIdx);
+});
+
+app.post('/next', (req, res) => {
+  //const id = req.body.id;//target question id
+  questionIdx = questionIdx + 1;
+  res.send(questionIdx);
+});
+
+app.post('/prev', (req, res) => {
+  questionIdx = questionIdx - 1;
+  res.send(questionIdx);
+});
+
+//---------------
 // socket server
 //---------------
 const server = http.createServer(app);
@@ -54,17 +74,9 @@ const wss = new WebSocket.Server({ server: server });
 wss.on('connection', (ws) => {
   console.log('[WS] socket connected');
 
-  // Send global message to Client in the schedule.
-  const sendNowTime = setInterval(() => {
-    ws.send(JSON.stringify({ text: BULLETS, avatar: USER_AVATAR }));
-    BULLETS = '';
-    USER_AVATAR = ''; // Refresh
-    console.log("interval triggered")
-  }, 2000);
-
   ws.on('message', function message(data) {
     data = data.toString()
-    console.log('[WS] received: '+ data);
+    console.log('[WS] received: ' + data);
     ws.send(data);//echo
   });
 
@@ -93,14 +105,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 //---------------
 // event handler
 //---------------
-async function handleEvent(event) {
+const handleEvent = async (event) => {
   console.log("Event : " + JSON.stringify(event));
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
 
-  const context = await event.message.text;
+  const context = event.message.text;
   if (context !== 'A' && context !== 'B' && context !== 'C' && context !== 'D') {
     // irrelavent response
     const echo = {
@@ -110,62 +122,72 @@ async function handleEvent(event) {
     return client.replyMessage(event.replyToken, echo);
   }
 
-  const user = await client.getProfile(event.source.userId);
-  const contentTime = event.message.timestamp
+  const userId = event.source.userId
+  //const contentTime = event.message.timestamp
+
+  await addUserIfNotExist(userId)
+  await updateUserAnswer(userId, context)
+
+  console.log("user pool: " + JSON.stringify(userPool));
+  console.log("answer pool: " + JSON.stringify(answerPool));
+
+  //reply user
   const echo = {
     type: 'text',
-    text: '問題' + (index +1) + '\n已收到你的答案: ' + context
+    text: '問題' + (questionIdx + 1) + '\n已收到你的答案: ' + context
   };
-
-  BULLETS  = context
-  USER_AVATAR = await user.pictureUrl;
-  console.log("get text: " + context + "\nfrom user: " + user + '\nuser pic: ' + USER_AVATAR)
-
   return client.replyMessage(event.replyToken, echo);
 }
 
-// const handleEvent = (event) => {
-//   console.log("Event : " + JSON.stringify(event));
+const addUserIfNotExist = (userId) => {
+  return new Promise(async (resolve, reject) => {
+    const idx = userPool.findIndex(item => item.id === userId);
+    if (idx > -1) {
+      //user exist
+      const foundUser = userPool[idx];
+      console.log("user found: " + JSON.stringify(foundUser))
+      resolve(foundUser)
+    } else {
+      //add new user
+      const user = await client.getProfile(userId);
+      const userAvatarUrl = user.pictureUrl;
+      const newUser = {
+        id: userId,
+        avatarUrl: userAvatarUrl
+      }
+      userPool.push(newUser);
+      resolve(newUser)
+    }
+  })
+}
 
-//   switch (event.type) {
-//     case 'join':
-//     case 'follow':
-//       break;
-//     case 'memberJoined':
-//       break;
-//     case 'message':
-//       if (event.message.type == 'text') {
-//         //get response
+const updateUserAnswer = async (userId, response) => {
+  return new Promise(async (resolve, reject) => {
+    let lastAnswer = answerPool[questionIdx].find(item => item.id === userId);
+    if (lastAnswer != undefined) {
+      //already answered, update answer
+      if (lastAnswer.ans != response) {
+        lastAnswer.ans = response
+        console.log("update answer to: " + response)
+        resolve(lastAnswer)
+      } else {
+        //same answer, no need update
+        console.log("same answer, no need update")
+        resolve()
+      }
+    } else {
+      //add new answer
+      const newAnswer = {
+        id: userId,
+        ans: response
+      }
+      answerPool[questionIdx].push(newAnswer)
+      console.log("add new answer: " + JSON.stringify(newAnswer))
+      resolve(newAnswer)
+    }
+  });
+}
 
-//         const senderId = event.source.userId
-//         const content = event.message.text
-//         const contentTime = event.message.timestamp
-//         console.log("get text: " + content + "\nfrom user: " + senderId)
-
-//         // Send  msg to user
-//         const dataToEmit = {
-//           message: content,
-//           username: senderId,
-//           __createdtime__: contentTime,
-//         }
-
-//         console.log("connections: " + connections)
-//         connections.forEach(client => {
-//           console.log("connections client: " + client)
-//           client.send(dataToEmit)
-//         });
-
-//         console.log("wss client num: " + wss.clients.size)
-//         wss.clients.forEach(client => {
-//           console.log("wss.clients client: " + client)
-//           client.send(dataToEmit);
-//        });
-
-//         return true;
-//       }
-//       break;
-//   }
-// }
 
 // Message event example
 
@@ -183,19 +205,3 @@ async function handleEvent(event) {
 //     "text": "1"
 //   }
 // }
-
-//{
-//  "type": "message",
-//  "replyToken": "6603176c7a1442908bd4b4ee6815d3e7",
-//  "source": {
-//    "groupId": "C779658f5ee9d3375736f89bd6c364440",
-//    "userId": "U911be356817fdf027cdda8352cea59cb",
-//    "type": "group"
-//  },
-//  "timestamp": 1540801146468,
-//  "message": {
-//    "type": "text",
-//    "id": "8785840232605",
-//    "text": "test"
-//  }
-//}
