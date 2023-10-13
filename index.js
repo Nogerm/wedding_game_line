4ecfe6283ffb3dec0584014c6b47bdff58b24d60
Andy CW Wu - LLX <andy_cw_wu@wistron.com>
2023-10-13 14:21:15 +0800

    fix comment code

--- index.js
+++ index.js
@@ -40,13 +40,13 @@ const answerArray = [
 // line sdk
 //---------------
 // create LINE SDK config from env variables
-// const config = {
-//   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-//   channelSecret: process.env.CHANNEL_SECRET,
-// };
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
 
 // create LINE SDK client
-// const client = new line.Client(config);
+const client = new line.Client(config);
 
 //---------------
 // express server
@@ -58,15 +58,15 @@ const app = express();
 //app.use(cors());
 
 // register a webhook handler with middleware
-// app.post('/webhook', line.middleware(config), (req, res) => {
-//   Promise
-//     .all(req.body.events.map(handleEvent))
-//     .then((result) => res.json(result))
-//     .catch((err) => {
-//       console.error(err);
-//       res.status(500).end();
-//     });
-// });
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
 
 // listen on port
 const port = process.env.PORT || 3000;
