ec5ef240a05e49638c2d5269a887ec9abbdc7b07
Andy CW Wu - LLX <andy_cw_wu@wistron.com>
2023-10-13 11:01:49 +0800

    update

--- index.js
+++ index.js
@@ -77,6 +77,11 @@ app.listen(port, () => {
 //---------------
 // api
 //---------------
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+});
 
 app.get('/qid', (req, res) => {
   console.log("[Express] Get qid: " + qId + "\nq total: " + qTotal);
